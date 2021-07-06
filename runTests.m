2612e204c097d975eac50c9d28542da6dd09f2c3
akeeste <akeeste@sandia.gov>
2021-07-06 16:33:31 -0600

    update testRunner

--- runTests.m
+++ runTests.m
@@ -6,11 +6,16 @@ function results = runTests(testsPath)
     
     import matlab.unittest.TestSuite;
     import matlab.unittest.TestRunner;
+    import matlab.unittest.plugins.*;
     
     suite = TestSuite.fromFolder(testsPath, ...
                                  'IncludingSubfolders', true);
     
+    % Create TestRunner
+    runner = TestRunner.withTextOutput; % Contains TestRunProgressPlugin, DiagnosticsOutputPlugin
+    runner.addPlugin(DiagnosticsRecordingPlugin);
+    
     % Run the tests
-    results = run(suite);
+    results = runner.run(suite);
     
 end
